c6cf113d14cd7a00f65d6c194683eb4dba0264ce
webgrafia <webgrafia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-02 07:44:25 +0000

    Release last version with fix for wp 3.1
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-tinymce-description/trunk@525955 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextgen-tinymce.php
+++ nextgen-tinymce.php
@@ -3,56 +3,57 @@
 Plugin Name: NextGen TinyMCE Picture Description
 Plugin URI: http://marbu.org/marbu/wordpress-plugin-tinymce-in-nextgen/
 Description: add TinyCME to NextGEN gallery picture description
-Author: Marco Buttarini
-Version: 1.1
-Author URI: http://marbu.og
+Author: Marco Buttarini & Giorgio Martello
+Version: 1.2
+Author URI: http://marbu.org
 */
 
+if( !empty($_SERVER['SCRIPT_FILENAME']) && __FILE__ == $_SERVER['SCRIPT_FILENAME']) { die('direct access not alowed '); }
 
-add_action("admin_head","load_custom_wp_tiny_mce");
-function load_custom_wp_tiny_mce() {
-if(($_GET['page'] == 'nggallery-manage-gallery')){
+if( $_GET['page'] == 'nggallery-manage-gallery' ){
+    
+    add_action('admin_footer', 'load_nextgen_tinymce');
+    
+    function load_nextgen_tinymce(){
+    ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function(){
+                
+                var elements=[];
+                
+                jQuery('textarea').filter(function() {
+                    return jQuery(this).attr('name').match(/description\[[0-9]{1,}\]+/);
+                }).each(function(k){
+                    
+                    var elem_id = 'description-'+jQuery(this).parent().parent().attr('id');
+                    jQuery(this).attr('id', elem_id );
+                    elements.push(elem_id);
+                    
+                });
+                
+                tinyMCE.init({
+                    mode : "exact",
+                    elements: elements.join(','),
+                    width:"100%",
+                    height:"200",
+                    theme:"advanced",
+                    skin:"default",
+                    language: tinyMCE.settings.language, // get setting from WP tinymce
+                    plugins : "fullscreen,inlinepopups,spellchecker", 
+                    theme_advanced_buttons1 : "bold,italic,underline,blockquote,separator,strikethrough,link,unlink",
+                    theme_advanced_buttons2 : "bullist,numlist,justifyleft,justifycenter,justifyright,undo,redo,fullscreen",
+                    theme_advanced_buttons3 : "",      
+                    theme_advanced_toolbar_location : "top",
+                    theme_advanced_toolbar_align : "left",
+                    theme_advanced_statusbar_location : "bottom",
+                    theme_advanced_resizing : true
+                    
+                });
+                
 
-}else return;
-?>
-<script>
-jQuery(document).ready(function(){
-	var texts = document.getElementsByTagName("textarea");
-	var elements=[];
-	for(var i=0;i<texts.length;i++){
-		if(/description\[[0-9]+\]/.test(texts[i].name)){
-			var elem_id = texts[i].name.replace("[", "_").replace("]","_");
-			texts[i].id = elem_id;
-			elements.push(elem_id);
-			
-		}
-	}
-	tinyMCEPreInit.mceInit.elements = elements.join(',');
-	tinyMCE.init(tinyMCEPreInit.mceInit);
-
-})
-</script>
-<?php
-	if (function_exists('wp_tiny_mce')) {
-		add_filter('teeny_mce_before_init', 'init_tinymce');
-		function init_tinymce($a){
-			
-			$a["theme"] = "advanced";
-			$a["skin"] = "wp_theme";
-			$a["height"] = "200";
-			$a["width"] = "100%";
-			//$a["onpageload"] = "";
-			$a["mode"] = "exact";
-			$a["elements"] = "";
-			$a["editor_selector"] = "theEditor";
-			$a["plugins"] = "fullscreen, inlinepopups, spellchecker";
-						
-			$a['theme_advanced_buttons1'] = "bold, italic, underline, blockquote, separator, strikethrough, link, unlink";
-			$a['theme_advanced_buttons2'] = "bullist, numlist,justifyleft, justifycenter, justifyright, undo, redo, fullscreen";
-
-			return $a;
-		}
-		wp_tiny_mce(true);
-	}
+            });
+        </script>
+    <?php
+    }
+    
 }
-?>
